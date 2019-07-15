5cb05cc95b9b4f4a634007a8efc4cf8d5af31eed
Stuart Kenny <skenny@tchpc.tcd.ie>
2019-07-15 16:11:44 +0100

    Add but do not show type facet

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -35,6 +35,7 @@ class CatalogController < ApplicationController
     config.add_facet_field  'readonly_grantee_ssim', label: 'Grantee'
     config.add_facet_field  'readonly_temporal_coverage_ssim', label: 'Year of Grant'
     config.add_facet_field  'readonly_geographical_coverage_ssim', label: 'Location of Grantee'
+    config.add_facet_field  'readonly_type_ssim', label: 'Type', show: false
 
     config.add_facet_fields_to_solr_request!
 
