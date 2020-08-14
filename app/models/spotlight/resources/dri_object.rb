87e51b0fbc7dea5bd91a4fc4202fe14b1817ede6
Stuart Kenny <skenny@tchpc.tcd.ie>
2020-08-14 12:36:05 +0100

    Add new grantees

--- app/models/spotlight/resources/dri_object.rb
+++ app/models/spotlight/resources/dri_object.rb
@@ -319,7 +319,11 @@ module Spotlight
                     "irish penal reform trust", "akidwa", "irish refugee council",
                     "south tyrone empowerment programme", "genio (organization)",
                     "glencree centre for reconciliation", "disability action northern ireland",
-                    "community foundation for northern ireland", "immigrant council of ireland"]
+                    "community foundation for northern ireland", "immigrant council of ireland",
+                    "northside partnership (dublin, ireland)", "ireland's age friendly cities and counties programme",
+                    "third age foundation", "the alzheimer society of ireland", "barnardos (Ireland)",
+                    "we the citizens-university college dublin foundation", "derry theatre trust",
+                    "marriage equality (organisation)"]
         COLLECTIONS = ["grant documentation", "oral histories", "publications"].freeze
 
         def initialize(metadata)
@@ -331,7 +335,7 @@ module Spotlight
         end
 
         def curated_collections
-          @curated_collections ||= metadata['subject'].select { |s| s.start_with?('Curated collection')}.map { |t| t.split('--')[1] }
+          @curated_collections ||= metadata['subject'].select { |s| s.start_with?('Curated collection')}.map { |t| t.split('--')[1..-1].join(' and ') }
         end
 
         def collection
