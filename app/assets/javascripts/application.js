7d69f46d3c6b649114e335d0effac542c6e88df9
Stuart Kenny <skenny@tchpc.tcd.ie>
2022-07-28 19:03:56 +0100

    stylesheet and javascript import fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,15 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
+//= require turbolinks
 //
 // Required by Blacklight
-//= require jquery3
-//= require jquery_ujs
 //= require popper
-//= require bootstrap-sprockets
+// Twitter Typeahead for autocomplete
+//= require twitter/typeahead
+//= require bootstrap
 //= require blacklight/blacklight
-//
+//= require blacklight_gallery
+
+
 //= require_tree .
 
 $(document).ready(function() {
